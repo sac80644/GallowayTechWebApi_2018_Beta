3889bba43cddf89c4a849756d01fcd6ab8c85b09
sac80644 <jason@gallowaytech.com>
2018-04-02 20:25:40 -0500

    Minor changes and versioning.

--- GallowayTechWebApi_2018_Beta/Global.asax.cs
+++ GallowayTechWebApi_2018_Beta/Global.asax.cs
@@ -1,16 +1,25 @@
-﻿using System.Data.Entity;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using GallowayTechWebApi_2018_Beta.Models;
+using System.Reflection;
+using System.IO;
 
 namespace GallowayTechWebApi_2018_Beta
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        public static string AssemblyVersionString;
+        public static string AssemblyBuildDateTimeString;
+
         protected void Application_Start()
         {
+            Assembly web = Assembly.Load("GallowayTechWebApi_2018_Beta");
+            AssemblyName webName = web.GetName();
+            AssemblyVersionString = webName.Version.ToString();
+            FileInfo fi = new FileInfo(web.Location);
+            AssemblyBuildDateTimeString = fi.CreationTime.ToString();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
